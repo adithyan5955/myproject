f0f06f0d9d7b075f889bdb32dd40bd163d3c1b6c
adithyan5955 <adithyan3875@gmail.com>
2023-09-05 11:16:55 +0530

    Initial commit for new project

---
+++ ecommerceproject/ecommerceproject/urls.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    
+    path('searchapp/', include('search_app.urls')),
+    path('cart/', include('cart.urls')),
+    path('', include('shop.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# print(urlpatterns)
